30b4625ac9831d561cb665246ffe576379d401f4
YngY <meng576256878@qq.com>
2017-10-24 14:19:56 +0800

    1.第一次提交

--- app/src/main/java/com/sky/dining/ui/MainActivity.java
+++ app/src/main/java/com/sky/dining/ui/MainActivity.java
@@ -43,6 +43,7 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         // TODO: add setContentView(...) invocation
         ButterKnife.bind(this);
     }
